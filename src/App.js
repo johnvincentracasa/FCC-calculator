eb61bb4da7025747f6cf30fa790027da674e464f
John Vincent Racasa <johnvincentracasa@gmail.com>
2021-10-24 22:11:06 +0800

    initial styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+
+      
+      <div className="container">
+
+        <div className="display">
+        678990
+        </div>
+
+        <button className="btn">1</button>
+        <button className="btn">2</button>
+        <button className="btn">3</button>
+        <button className="btn">4</button>
+
+        <button className="btn">1</button>
+        <button className="btn">2</button>
+        <button className="btn">3</button>
+        <button className="btn">4</button>
+
+        <button className="btn">1</button>
+        <button className="btn">2</button>
+        <button className="btn">3</button>
+        <button className="btn">4</button>
+
+        <button className="btn">1</button>
+        <button className="btn">2</button>
+        <button className="btn">3</button>
+        <button className="btn">4</button>
+
+        <button className="btn">1</button>
+        <button className="btn">2</button>
+        <button className="btn">3</button>
+        <button className="btn">4</button>
+      </div>
     </div>
   );
 }
